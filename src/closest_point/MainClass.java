d5bfef2cc9307cfd30f3c1fa784d1135c213b12f
isilkrt <aisilkurt@gmail.com>
2017-10-31 17:40:11 +0300

    change float to double. change write function.

--- src/closest_point/MainClass.java
+++ src/closest_point/MainClass.java
@@ -1,6 +1,7 @@
 package closest_point;
 
 import java.lang.*;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -36,7 +37,7 @@ public class MainClass {
 
 		final long duration = System.nanoTime() - startTime; 	//keep end time of system.
 		System.out.print("Run time of system : ");
-		System.out.print( (float) duration / 1000000 );
+		System.out.print( (double) duration / 1000000 );
 		System.out.print(" milliseconds.");
 
 	}
@@ -55,7 +56,7 @@ public class MainClass {
 		Point[] point_list = new Point[numberOfPoints];
 		point_list = point.toArray(point_list);
 
-		finald.setDistance(Float.MAX_VALUE);			//sets minimum distance as maximum float  number.
+		finald.setDistance(Double.MAX_VALUE);			//sets minimum distance as maximum float  number.
         finald = divide_conquer(point_list, 0);			//run divide_conquer function
 
         try{
@@ -133,16 +134,16 @@ public class MainClass {
 			Distance sec_min =  divide_conquer(right_side, axis);
 			Distance min_divide_part = first_min.distance_min_distance(sec_min);
 
-			float divide_distance = min_divide_part.getDistance();
-			float finald_distance = finald.getDistance();
+			double divide_distance = min_divide_part.getDistance();
+			double finald_distance = finald.getDistance();
 			if(divide_distance < finald_distance){
 				finald = new Distance(divide_distance, min_divide_part.getA(), min_divide_part.getB());
 			}
 
 			/* Find median of used axis. */
-			float med1 = left_side[left_side.length-1].getCoordinates()[axis];
-			float med2 = right_side[0].getCoordinates()[axis];
-			float median = (Float)(Math.abs(med1 + med2))/2;
+			double med1 = left_side[left_side.length-1].getCoordinates()[axis];
+			double med2 = right_side[0].getCoordinates()[axis];
+			double median = (Double)(Math.abs(med1 + med2))/2;
 
 			/* Point list oluştur to keeps points that are in intersection area of two divided parts.
 			 * Starts conquer part.
@@ -185,7 +186,7 @@ public class MainClass {
 						Point second = conquer_poins[i];	//sample point
 						Distance cc = new Distance(first, second);
 
-						float closeness = cc.getDistance();
+						double closeness = cc.getDistance();
 						finald_distance = finald.getDistance();
 
 						if(finald_distance > closeness){
@@ -201,7 +202,7 @@ public class MainClass {
 				// Change dimension and call divide conquer algorithm for new list.
 				Distance space_min = divide_conquer(conquer_poins, axis+1);
 
-				float closeness = space_min.getDistance();
+				double closeness = space_min.getDistance();
 				finald_distance = finald.getDistance();
 				if(closeness < finald_distance){
 					finald = space_min;
@@ -231,11 +232,11 @@ public class MainClass {
 
 			String[] listOfDimension = line.split("\t");
 			dimension = listOfDimension.length;
-			Float[] float_dimensions = new Float[dimension];  	// dimension values of point
+			Double[] float_dimensions = new Double[dimension];  	// dimension values of point
 
 			int count = 0;	// axis - 0 mean x axis
 			for (String num: listOfDimension) {
-				float_dimensions[count] = Float.parseFloat(num);
+				float_dimensions[count] = Double.parseDouble(num);
 				count ++;
 			}
 
@@ -262,11 +263,6 @@ public class MainClass {
 		if(finald==null){
 			System.out.println("There are not enough point. Zero or one.");
 		}else{
-			DecimalFormat df = new DecimalFormat("#"); 
-			df.setMaximumIntegerDigits(6);
-			df.setMinimumIntegerDigits(1);
-			df.setMaximumFractionDigits(1);
-			df.setGroupingSize(6);
 
 			//process to write output file
 			String file = "sample_output_" + dimension +"_" + numberOfPoints + ".txt" ;
@@ -281,7 +277,9 @@ public class MainClass {
 			writer.print(finald.getA().getOrder());
 			writer.print(":");
 			for(int i = 0; i<dimension; i++){
-				String h = df.format(finald.getA().getCoordinates()[i]).replace(',', '.');
+				String h = round_double(finald.getA().getCoordinates()[i]);
+			    //String h = df.format(finald.getA().getCoordinates()[i]).replace(',', '.');
+				//System.out.println(h + "*");
 				writer.print(h + "\t");
 			}
 
@@ -289,11 +287,97 @@ public class MainClass {
 			writer.print(finald.getB().getOrder());
 			writer.print(":");
 			for(int i = 0; i<dimension; i++){
-				String h = df.format(finald.getB().getCoordinates()[i]).replace(',', '.');
-				writer.print(h + "\t");
+				String h = round_double(finald.getB().getCoordinates()[i]);
+			    writer.print(h + "\t");
 			}
 
 			writer.close();
 		}
+	}
+
+	public static String round_double(double m ){
+		DecimalFormat df = new DecimalFormat("#.##");
+		df.setMaximumIntegerDigits(6);
+		df.setMinimumIntegerDigits(1);
+		df.setMaximumFractionDigits(2);
+		df.setMinimumFractionDigits(2);
+		//df.setRoundingMode(RoundingMode.CEILING);
+		String mm = df.format(m);
+		mm = mm.replace(",", ".");
+
+		String delims = "[.]+";
+		String[] tokens = mm.split(delims);
+		String integer = tokens[0];
+		String fraction = null;
+		char first = 'a';
+		char second = 'b';
+		if (tokens.length == 2){
+			fraction = tokens[1];
+			first = fraction.charAt(0);
+			second = fraction.charAt(1);
+		}
+
+		String final_string = "";
+		char a_char = integer.charAt(0);
+		if(a_char == '-'){
+			if(integer.length() == 7){
+				if(tokens.length == 2){
+					int x = Character.getNumericValue(first);
+					if(x<5){
+						final_string = final_string + integer;
+					}else if(x == 9){
+						final_string = final_string + integer;
+					}else{
+						//count = 0;
+						for(int n = 0; n<integer.length()-1; n++){
+							final_string = final_string + integer.charAt(n);
+							//count ++;
+						}
+						int xv = Character.getNumericValue(integer.charAt(6));
+						xv = xv+1;
+						final_string = final_string + xv;
+					}
+				}else{
+					final_string = final_string + integer;
+				}
+			}else{
+				if(tokens.length == 2){
+					final_string = final_string +integer + "." + fraction;
+				}else{
+					final_string = final_string +integer;
+				}
+
+			}
+		}else{
+			if(integer.length() == 6){
+				if(tokens.length == 2){
+					int x = Character.getNumericValue(fraction.charAt(1));
+					if(x<5){
+						final_string = final_string + integer;
+					}else if(x == 9){
+						final_string = final_string + integer;
+					}else{
+						//count = 0;
+						for(int n = 0; n<integer.length()-1; n++){
+							//writer.print(integer.charAt(n));
+							final_string = final_string + integer.charAt(n);
+							//count ++;
+						}
+						int xv = Character.getNumericValue(integer.charAt(6));
+						xv = xv+1;
+						final_string = final_string + xv;
+					}
+				}else{
+					final_string = final_string + integer;
+				}
+			}else{
+				if(tokens.length == 2){
+					final_string = final_string + integer + "." + fraction.charAt(0);
+				}else{
+					final_string = final_string + integer;
+				}
+			}
+		}
+		return final_string;
 	}
 }
